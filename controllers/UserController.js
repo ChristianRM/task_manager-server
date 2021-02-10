74ef0d02c60316e4f4fc0a41faa1cf5e73b420cb
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-02-09 19:17:01 -0600

    Add register new user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,3 +1,56 @@
-exports.createUser = (req, res) => {
-    console.log(req.body)
+const User = require('../models/User')
+const bcryptjs = require('bcryptjs')
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+
+exports.createUser = async (req, res) => {
+    // Revisar si hay errores
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    // Extraer email y password
+    const { email, password } = req.body
+
+    try {
+        let user = await User.findOne({ email })
+        if (user)
+            return res.status(400).json({ msg: 'User already exists' })
+
+        // Crea usuario
+        user = new User(req.body)
+
+        // Hash al password
+        const salt = await bcryptjs.genSalt(10)
+
+        user.password = await bcryptjs.hash(password, salt)
+
+        // Guardar usuario
+        await user.save()
+
+        // Crear y firmar el jwt
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        // Firmar el jwt
+        jwt.sign(payload, process.env.SECRET, {
+            expiresIn: 3600
+        }, (error, token) => {
+            if (error){
+                throw error
+            }
+            res.json({ token })
+        })
+
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).send('Hubo un error')
+
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,17 @@
 const express = require('express')
 const router = express.Router()
 const UserController = require('../controllers/UserController')
+const { check } = require('express-validator')
 
 // Crea un usuario
 // api/usuarios
-router.post('/', 
-UserController.createUser
+router.post('/',
+    [
+        check('name', 'Name field is needed').not().isEmpty(),
+        check('email', 'Please enter a valid email address').isEmail(),
+        check('password', 'Password must be at least 8 characters long').isLength({ min: 8, max: 16 })
+    ],
+    UserController.createUser
 )
 
 module.exports = router
