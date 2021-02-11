1b010c2b339886c503a0645009f38dcefec41084
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-02-10 19:00:21 -0600

    Add new project validation

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -1,5 +1,13 @@
 const Project = require('../models/Project')
+const { validationResult } = require('express-validator')
+
 exports.createProject = async (req, res) => {
+    // Revisar si hay errores
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
     try {
         // Crear un nuevo proyecto
         const project = new Project(req.body)
--- routes/projects.js
+++ routes/projects.js
@@ -2,11 +2,15 @@ const express = require('express')
 const router = express.Router()
 const projectController = require('../controllers/ProjectController')
 const auth = require('../middleware/auth')
+const { check } = require('express-validator')
 
 // Crea proyectos
 // api/projects
 router.post('/',
     auth,
+    [
+        check('name', 'Name field is needed').not().isEmpty()
+    ],
     projectController.createProject
 )
 
